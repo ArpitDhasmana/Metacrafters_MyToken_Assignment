8200544ae2432d4aadaef9871f481f429d545581
ArpitDhasmana <129339943+ArpitDhasmana@users.noreply.github.com>
2024-05-28 16:46:00 +0530

    Add files via upload

---
+++ Mytoken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+
+contract MyToken {
+
+    // public variables here
+    string public TK_Name="Anna";
+    string public TK_abbrv="ana";
+    uint public totalSupply=0;
+
+    // mapping variable here
+    mapping (address=>uint) balance;
+
+    // mint function
+    function mint(address _add,uint _val) public{
+        totalSupply+=_val;
+        balance[_add]+=_val;
+    }
+
+    // burn function
+     function burn(address _add,uint _val) public{
+        totalSupply-=_val;
+        if(balance[_add]>=_val){
+        balance[_add]-=_val;
+        }
+    }
+}
